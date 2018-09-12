2ea402f8997df038ea948b07aca14c67436914af
yogaraja gopal <gyogarajaeee@gmail.com>
2018-09-12 22:33:53 +0100

    changes added 20180912

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://yogaraja:YOga7276@ds155352.mlab.com:55352/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
